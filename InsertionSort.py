afc226b240950dcdfb8451e1befcbcd9a62fe819
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2023-01-27 08:26:35 +0800

    Fixed Display Error & Replaced InSort Function

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,19 +1,16 @@
 # -------------------- INSERTION SORT --------------------------
-# Code snippet from programiz.com
+# Code snippet from geeksforgeeks.org
 
 def insertionSort(array):
-
-    for step in range(1, len(array)):
-        key = array[step]
-        j = step - 1
-
-        # Compare key with each element on the left of it until an element smaller than it is found
-        while j >= 0 and key < array[j]:
-            array[j + 1] = array[j]
-            j = j - 1
-        
-        # Place key at after the element just smaller than it.
-        array[j + 1] = key
+    if (n := len(array)) <= 1:
+      return
+    for i in range(1, n):
+        key = array[i]
+        j = i-1
+        while j >=0 and key < array[j] :
+                array[j+1] = array[j]
+                j -= 1
+        array[j+1] = key
 
 data = [93, 57, 76, 21, 95, 69, 51, 74, 38, 63]
 
@@ -21,4 +18,6 @@ print("\n\u001b[36mUnsorted Array\u001b[0m")
 print(data)
 
 print("\n\u001b[32mSorted Array\u001b[0m in Ascending Order:")
-print(insertionSort(data), "\n")
+
+insertionSort(data)
+print(data, "\n")
