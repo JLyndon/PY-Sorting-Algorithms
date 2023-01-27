e3441dcad11ff3aff647178eae934dc00f11889a
DESKTOP-VM4D9GJ <jolyndonjr@gmail.com>
2023-01-27 08:47:30 +0800

    Added Function to find partition position

---
+++ QuickSort.py
@@ -0,0 +1,25 @@
+# -------------------- QUICK SORT --------------------------
+# Code snippet from geeksforgeeks.org
+
+# Function to find the partition position
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+
+            # If element smaller than pivot is found
+            # swap it with the greater element pointed by i
+            i = i + 1
+ 
+            # Swapping element at i with element at j
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    # Swap the pivot element with the greater element specified by i
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+ 
+    # Return the position from where partition is done
+    return i + 1
+ 
+
